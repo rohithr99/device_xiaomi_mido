12ed17f9c18c69d5f88c81a8a085ce9e3ddbc7e7
ThatisJigen <gianfranco.liguori.96@gmail.com>
2020-01-27 17:41:24 +0100

    AEX-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mido.mk
+    $(LOCAL_DIR)/aosp_mido.mk
 
-COMMON_LUNCH_CHOICES := \
-    lineage_mido-user \
-    lineage_mido-userdebug \
-    lineage_mido-eng
+COMMON_LUNCH_CHOICES :=  \
+    aosp_mido-userdebug
