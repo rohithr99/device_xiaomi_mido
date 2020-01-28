1d0ee37c840508f4fe3d8da16a417fdc07dc352d
ZeeLog <Danjel.Zammit@gmail.com>
2020-01-28 13:44:39 +0100

    mido: Update Vendor SPL
    Change-Id: I41b0a8f0c6a156e2bd9b39dc81aa5d6f4a969c03

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,7 +185,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_USES_OLD_MNC_FORMAT := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-05-05
+VENDOR_SECURITY_PATCH := 2020-01-01
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
